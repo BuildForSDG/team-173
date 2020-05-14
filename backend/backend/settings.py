2098da317ba0731d0a714df426bf4b55cd7209c0
Nyaribari Reuben <anyaribari@gmail.com>
2020-05-14 18:38:14 +0300

    migrating to postgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,8 +105,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'give2get',
+        'USER':'reuben',
+        'PASSWORD':'give2get2020',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
